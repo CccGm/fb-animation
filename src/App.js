28c903fc8084653bcfcf7330ca98a0f53a431fb2
kaushik-godhani <kaushikgodhani2218@gmail.com>
2024-07-08 17:41:07 +0530

    vertical scroller Frame6

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Frame2 } from "./components/page/Frame2";
 import { Frame3 } from "./components/page/Frame3";
 import { Frame4 } from "./components/page/Frame4/Frame4";
 import { Frame5 } from "./components/page/Frame5/Frame5";
-import { Frame6 } from "./components/page/Frame6";
+import { Frame6 } from "./components/page/Frame6/Frame6";
 import { Frame7 } from "./components/page/Frame7";
 import { Frame8 } from "./components/page/Frame8";
 import { Frame9 } from "./components/page/Frame9/Frame9";
@@ -19,14 +19,14 @@ function App() {
         {/* <Frame1 /> */}
         {/* <Frame2 /> */}
         {/* <Frame3 /> */}
-        <Frame4 />
+        {/* <Frame4 /> */}
         {/* <Frame5 /> */}
-        {/* <Frame6 /> */}
+        <Frame6 />
         {/* <Frame7 /> */}
         {/* <Frame8 /> */}
         {/* <Frame9 /> */}
         {/* <Frame10 /> */}
-        {/* <Test /> */}
+        <Test />
       </AppProvider>
     </div>
   );
---
+++ src/components/page/Frame6/Frame6.js
@@ -0,0 +1,68 @@
+import React from "react";
+import phone from "../../../assets/images/page6/phone.png";
+import movie from "../../../assets/images/page6/movies cd.png";
+import camera from "../../../assets/images/page6/camera man.png";
+import lady from "../../../assets/images/page6/ladie.png";
+import "./Frame6.css";
+
+export const Frame6 = () => {
+  return (
+    <div className="w-full bg-black grid grid-cols-2 gap-3 h-screen">
+      <div className="grid grid-cols-2 h-screen gap-3">
+        <div className="overflow-hidden">
+          <div class="scroll-container">
+            <div class="scroll-content scroll-content-up">
+              <img src={phone} alt="Image 1" />
+              <img src={camera} alt="Image 2" />
+              <img src={movie} alt="Image 3" />
+              <img src={lady} alt="Image 4" />
+
+              <img src={phone} alt="Image 1" />
+              <img src={camera} alt="Image 2" />
+              <img src={movie} alt="Image 3" />
+              <img src={lady} alt="Image 4" />
+            </div>
+          </div>
+        </div>
+        <div className="overflow-hidden ">
+          <div class="scroll-container">
+            <div class="scroll-content scroll-content-down">
+              <img src={phone} alt="Image 1" />
+              <img src={camera} alt="Image 2" />
+              <img src={movie} alt="Image 3" />
+              <img src={lady} alt="Image 4" />
+
+              <img src={phone} alt="Image 1" />
+              <img src={camera} alt="Image 2" />
+              <img src={movie} alt="Image 3" />
+              <img src={lady} alt="Image 4" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-full flex items-center justify-center bg-app-orange">
+        <div>
+          <p
+            className=" underline"
+            style={{
+              color: "#5A0FE8",
+              fontFamily: "Feeling",
+              fontSize: 24,
+            }}
+          >
+            FuzzyBox
+          </p>
+          <p
+            style={{
+              color: "#ffffff",
+              fontFamily: "Geometric",
+              fontSize: 128,
+            }}
+          >
+            GOSSIP <br /> GAZETTE
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
